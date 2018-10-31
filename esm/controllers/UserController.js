7cfadab1e61f0306d1e9cdfd52cecccf3dffa50d
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-10-31 11:01:12 -0300

    Add sign-up route
    This patch also sets up all the basic service infrastructure:
    - Web server constructed using HapiJS
    - Configuration using 'config' package
    - Logging using Winston
    
    Change-Id: I8446c68ca6b1efcc091ce0127747e333faf0ffd4

---
+++ esm/controllers/UserController.js
@@ -0,0 +1,38 @@
+import Boom from 'boom';
+
+import InvalidPasswordError from 'entities/InvalidPasswordError';
+import UserExistsError from 'entities/UserExistsError';
+
+class UserController {
+  constructor(createUserInteractor, logger) {
+    this.createUserInteractor = createUserInteractor;
+    this.logger = logger;
+  }
+
+  async create(request, h) {
+    try {
+      const { email, password } = request.payload;
+      const user = await this.createUserInteractor.execute(email, password);
+      return h.response(this.mapUserToJson(user)).code(201);
+    } catch (error) {
+      if (error instanceof InvalidPasswordError) {
+        throw Boom.boomify(error, { statusCode: 400 });
+      }
+      if (error instanceof UserExistsError) {
+        throw Boom.boomify(error, { statusCode: 409 });
+      }
+      this.logger.error(`Unexpected error at create(): ${error.message}`);
+      throw Boom.internal();
+    }
+  }
+
+  mapUserToJson(user) {
+    return {
+      email: user.email,
+      uuid: user.uuid,
+      token: user.token,
+    };
+  }
+}
+
+export default UserController;
