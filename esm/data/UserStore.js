2198c96db3c5f842a138c837c3195c7cfb1d1b3a
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-11-01 20:30:36 -0300

    Add forgot password route
    Change-Id: I9c03b0e4c52538a38ca43feef795eb6bdc394ea8

--- esm/data/UserStore.js
+++ esm/data/UserStore.js
@@ -1,15 +1,23 @@
+import _ from 'lodash';
+import moment from 'moment';
+
 import User from 'entities/User';
 import UserExistsError from 'entities/UserExistsError';
 
 class UserStore {
-  constructor(meshbluAuthenticator) {
+  constructor(meshbluAuthenticator, meshbluHttp) {
     this.meshbluAuthenticator = meshbluAuthenticator;
+    this.meshbluHttp = meshbluHttp;
+  }
+
+  async get(email) {
+    const device = await this.getDevice(email);
+    return device && this.mapDeviceToUser(device);
   }
 
   async create(email, password) {
     return new Promise((resolve, reject) => {
-      const query = {};
-      query[`${this.meshbluAuthenticator.authenticatorUuid}.id`] = email;
+      const query = this.buildQuery(email);
       const userParams = {
         query,
         data: {
@@ -20,15 +28,102 @@ class UserStore {
       };
       this.meshbluAuthenticator.create(userParams, (error, userDevice) => {
         if (error) {
-          reject(this.parseError(error, email));
+          reject(this.mapError(error, email));
         } else {
-          resolve(new User(email, userDevice.uuid, userDevice.token));
+          resolve(this.mapDeviceToUser(userDevice));
         }
       });
     });
   }
 
-  parseError(error, email) {
+  async update(user) {
+    const device = await this.getDevice(user.email);
+    this.updateDeviceWithUser(device, user);
+    return new Promise((resolve, reject) => {
+      this.meshbluHttp.update(device.uuid, device, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  async getDevice(email) {
+    return new Promise((resolve, reject) => {
+      const query = this.buildQuery(email);
+      this.meshbluHttp.devices(query, (error, devices) => {
+        if (error) {
+          reject(this.mapError(error));
+          return;
+        }
+
+        const device = _.chain(devices)
+          .filter(this.isValid.bind(this))
+          .first() // shouldn't exist more than one
+          .value();
+        resolve(device);
+      });
+    });
+  }
+
+  buildQuery(email) {
+    const query = {};
+    query[`${this.meshbluAuthenticator.authenticatorUuid}.id`] = email;
+    return query;
+  }
+
+  getAuthData(device) {
+    return device[this.meshbluAuthenticator.authenticatorUuid];
+  }
+
+  setAuthData(device, authData) {
+    // eslint-disable-next-line no-param-reassign
+    device[this.meshbluAuthenticator.authenticatorUuid] = authData;
+  }
+
+  isValid(device) {
+    const authData = this.getAuthData(device);
+    if (!authData || !authData.signature) {
+      return false;
+    }
+    return this.meshbluHttp.verify(
+      _.omit(authData, 'signature'),
+      authData.signature,
+    );
+  }
+
+  sign(authData) {
+    const signedAuthData = _.cloneDeep(authData);
+    signedAuthData.signature = this.meshbluHttp.sign(_.omit(signedAuthData, 'signature'));
+    return signedAuthData;
+  }
+
+  updateDeviceWithUser(device, user) {
+    const authData = this.getAuthData(device);
+    authData.resetExpiration = user.resetExpiration
+      ? user.resetExpiration.toJSON()
+      : undefined;
+    authData.resetToken = user.resetToken;
+    this.setAuthData(device, this.sign(authData));
+  }
+
+  mapDeviceToUser(device) {
+    const authData = this.getAuthData(device);
+    const resetExpiration = authData.resetExpiration
+      ? moment(authData.resetExpiration)
+      : undefined;
+    return new User(
+      authData.id,
+      device.uuid,
+      device.token,
+      authData.resetToken,
+      resetExpiration,
+    );
+  }
+
+  mapError(error, email) {
     if (error.message === 'device already exists') {
       return new UserExistsError(email);
     }
---
+++ esm/interactors/ForgotPassword.js
@@ -0,0 +1,55 @@
+import moment from 'moment';
+import UUID from 'uuid';
+import bcrypt from 'bcrypt';
+import util from 'util';
+
+const EXPIRATION_IN_MINUTES = 30;
+const FROM_ADDRESS = 'knot@cesar.org.br';
+
+const hashAsync = util.promisify(bcrypt.hash);
+
+class ForgotPassword {
+  constructor(userStore, mailService, resetUri) {
+    this.userStore = userStore;
+    this.mailService = mailService;
+    this.resetUri = resetUri;
+  }
+
+  async execute(email) {
+    const user = await this.userStore.get(email);
+    if (!user) {
+      return;
+    }
+
+    const resetToken = this.createResetToken();
+    user.resetToken = await this.hashToken(user, resetToken);
+    user.resetExpiration = this.getResetExpirationDate();
+    await this.userStore.update(user);
+
+    try {
+      await this.sendMail(user, resetToken);
+    } catch (error) {
+      // Ignore mail errors
+    }
+  }
+
+  createResetToken() {
+    return UUID.v4();
+  }
+
+  async hashToken(user, token) {
+    return hashAsync(token + user.uuid, 10);
+  }
+
+  getResetExpirationDate() {
+    return moment().add(EXPIRATION_IN_MINUTES, 'minutes');
+  }
+
+  async sendMail(user, token) {
+    const resetUri = `${this.resetUri}?uuid=${user.uuid}&token=${token}`;
+    const body = `<p>You recently requested to reset your password for your KNoT Cloud account. Click <a href="${resetUri}">here</a> to reset your password, it will be available for the next ${EXPIRATION_IN_MINUTES} minutes.</p><p>If you didn't make this request, please ignore this e-mail.`;
+    await this.mailService.sendHtml(FROM_ADDRESS, user.email, 'Forgot password?', body);
+  }
+}
+
+export default ForgotPassword;
