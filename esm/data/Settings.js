ee7ce5e7518f24d1b3144e5f828aba744cde6f83
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-11-01 20:30:36 -0300

    Add forgot password route
    Change-Id: I9c03b0e4c52538a38ca43feef795eb6bdc394ea8
    Reviewed-on: https://review.gerrithub.io/431718
    Reviewed-by: João Neto <joao.alexandre@cesar.org.br>
    Reviewed-by: Paulo Serra Filho <ptvsf@cesar.org.br>
    Reviewed-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>
    Tested-by: Thiago Cardoso <thiago.figueredo@cesar.org.br>

--- esm/data/Settings.js
+++ esm/data/Settings.js
@@ -1,8 +1,9 @@
 class Settings {
-  constructor(server, meshblu, authenticator, logger) {
+  constructor(server, meshblu, authenticator, mailgun, logger) {
     this.server = server;
     this.meshblu = meshblu;
     this.authenticator = authenticator;
+    this.mailgun = mailgun;
     this.logger = logger;
   }
 }
