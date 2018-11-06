bdf091d5aa6ade902bc68c2cc1a3091f8a9ac434
Thiago Cardoso <thiago.figueredo@cesar.org.br>
2018-11-06 15:18:16 -0300

    Add reset password route
    Change-Id: I1f4d08c831e28432a4835ed6973824a8ed1c5a10

--- esm/controllers/UserController.js
+++ esm/controllers/UserController.js
@@ -2,11 +2,13 @@ import Boom from 'boom';
 
 import InvalidPasswordError from 'entities/InvalidPasswordError';
 import UserExistsError from 'entities/UserExistsError';
+import InvalidResetTokenError from 'interactors/InvalidResetTokenError';
 
 class UserController {
-  constructor(createUserInteractor, forgotPasswordInteractor, logger) {
+  constructor(createUserInteractor, forgotPasswordInteractor, resetPasswordInteractor, logger) {
     this.createUserInteractor = createUserInteractor;
     this.forgotPasswordInteractor = forgotPasswordInteractor;
+    this.resetPasswordInteractor = resetPasswordInteractor;
     this.logger = logger;
   }
 
@@ -34,6 +36,21 @@ class UserController {
       return h.response().code(200);
     } catch (error) {
       this.logger.error(`Unexpected error at forgot(): ${error.message}`);
+      throw Boom.internal();
+    }
+  }
+
+  async reset(request, h) {
+    try {
+      const { uuid, token, password } = request.payload;
+      await this.resetPasswordInteractor.execute(uuid, token, password);
+      return h.response().code(200);
+    } catch (error) {
+      if (error instanceof InvalidPasswordError
+        || error instanceof InvalidResetTokenError) {
+        throw Boom.boomify(error, { statusCode: 400 });
+      }
+      this.logger.error(`Unexpected error at reset(): ${error.message}`);
       throw Boom.internal();
     }
   }
