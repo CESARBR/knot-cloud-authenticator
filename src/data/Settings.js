f0d4cac014d9b9a08ed4349571118391e2f01598
Ramon Ribeiro <rhpr@cesar.org.br>
2019-03-19 09:41:48 -0300

    Add webhook to router allowing forward messages
    This patch allows the authenticator forward messages to storage service
    
    Change-Id: Id031d7c80b777e225c9289f407c183bb7286f80a

--- src/data/Settings.js
+++ src/data/Settings.js
@@ -1,10 +1,11 @@
 class Settings {
-  constructor(server, meshblu, authenticator, mailgun, logger) {
+  constructor(server, meshblu, authenticator, mailgun, logger, storage) {
     this.server = server;
     this.meshblu = meshblu;
     this.authenticator = authenticator;
     this.mailgun = mailgun;
     this.logger = logger;
+    this.storage = storage;
   }
 }
 
