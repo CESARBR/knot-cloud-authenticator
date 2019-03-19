0c7f38f6f1444c8b9df97b190bc06df412e9f81a
Ramon Ribeiro <rhpr@cesar.org.br>
2019-03-19 09:41:48 -0300

    Add webhook to router allowing forward messages
    This patch allows the authenticator forward messages to storage service
    
    Change-Id: Id031d7c80b777e225c9289f407c183bb7286f80a

--- src/data/SettingsFactory.js
+++ src/data/SettingsFactory.js
@@ -30,6 +30,9 @@ const mailgunSchema = Joi.object().keys({
   apiKey: Joi.string().required(),
   domain: Joi.string().required(),
 });
+const storageSchema = Joi.object().keys({
+  webhookUri: Joi.string().uri(),
+});
 const levels = ['error', 'warn', 'info', 'verbose', 'debug', 'silly'];
 const loggerSchema = Joi.object().keys({
   level: Joi.string().valid(levels).required(),
@@ -42,7 +45,8 @@ class SettingsFactory {
     const authenticator = this.loadAuthenticatorSettings();
     const mailgun = this.loadMailgunSettings();
     const logger = this.loadLoggerSettings();
-    return new Settings(server, meshblu, authenticator, mailgun, logger);
+    const storage = this.loadStorageSettings();
+    return new Settings(server, meshblu, authenticator, mailgun, logger, storage);
   }
 
   loadServerSettings() {
@@ -62,6 +66,7 @@ class SettingsFactory {
       protocol: meshblu.protocol,
       hostname: meshblu.hostname || meshblu.host,
       port: meshblu.port,
+      webhookUri: meshblu.webhookUri,
     };
   }
 
@@ -81,6 +86,12 @@ class SettingsFactory {
     const logger = config.get('logger');
     this.validate('logger', logger, loggerSchema);
     return logger;
+  }
+
+  loadStorageSettings() {
+    const storage = config.get('storage');
+    this.validate('storage', storage, storageSchema);
+    return storage;
   }
 
   validate(propertyName, propertyValue, schema) {
